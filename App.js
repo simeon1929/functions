86f73d87e4cd0f7342536e97bfc8ad874adcb4cd
HR <hr@batch.lan>
2023-05-10 10:10:36 +0500

    simple function started

---
+++ App.js
@@ -0,0 +1,10 @@
+console.log('Functions');
+
+// simple fuctions
+function test(wifename, age){
+    console.log(`Hello simeon and your wife name is ', ${wifename} , your age is ${age} `);
+   
+}
+
+test('beulah', 35)
+
